f0f45d43f914421538bf3709f4fc03967c084511
ulricolo7 <ulricoco39lo7@gmail.com>
2024-10-31 10:26:17 +0800

    Update Character Movement

--- Script/player-character.gd
+++ Script/player-character.gd
@@ -1,32 +1,72 @@
 extends CharacterBody2D
 
 var STEP = 50
-var start_pos = position
-var LEFT_BOUNDARY = start_pos.x + 50
-var RIGHT_BOUNDARY = start_pos.x + 1350
+
+var LEFT_BOUNDARY = 50
+var RIGHT_BOUNDARY = 1350
+var TOP_BOUNDARY = 50
+var BOTTOM_BOUNDARY = 750
 
 var char_just_moved = false
 
+var DIRECTIONS = ["UP", "RIGHT", "DOWN", "LEFT"]
+
+var curr_direction = DIRECTIONS[3]
+
+@onready
+var anim_sprite = $AnimatedSprite2D
+
 func _ready():
-	$AnimatedSprite2D.play("default")
+	idle()
 
 func _process(delta):
-	
 	if char_just_moved:
 		pass
-	elif Input.is_action_pressed("move_down"):
-		position.y += STEP
-		char_just_moved = true
-	elif Input.is_action_pressed("move_up"):
-		position.y -= STEP
-		char_just_moved = true
+	elif Input.is_action_pressed("move_down") && position.y != BOTTOM_BOUNDARY:
+		move_down()
+	elif Input.is_action_pressed("move_up") && position.y != TOP_BOUNDARY:
+		move_up()
 	elif Input.is_action_pressed("move_left") && position.x != LEFT_BOUNDARY:
-		position.x -= STEP
-		char_just_moved = true
+		move_left()
 	elif Input.is_action_pressed("move_right") && position.x != RIGHT_BOUNDARY:
-		position.x += STEP
-		char_just_moved = true
+		move_right()
 	
 	if !Input.is_anything_pressed():
-		char_just_moved = false
+		idle()
+		
 
+func move_up():
+	curr_direction = DIRECTIONS[0]
+	anim_sprite.play("run_up")
+	position.y -= STEP
+	char_just_moved = true
+	
+func move_right():
+	curr_direction = DIRECTIONS[1]
+	anim_sprite.flip_h = false
+	anim_sprite.play("run")
+	position.x += STEP
+	char_just_moved = true
+	
+func move_down():
+	curr_direction = DIRECTIONS[2]
+	anim_sprite.play("run_down")
+	position.y += STEP
+	char_just_moved = true
+
+func move_left():
+	curr_direction = DIRECTIONS[3]
+	anim_sprite.flip_h = true
+	anim_sprite.play("run")
+	position.x -= STEP
+	char_just_moved = true
+
+func idle():
+	if curr_direction == DIRECTIONS[0]:
+		anim_sprite.play("idle_up")
+	elif curr_direction == DIRECTIONS[2]:
+		anim_sprite.play("idle_down")
+	else:
+		anim_sprite.play("idle")
+		
+	char_just_moved = false
