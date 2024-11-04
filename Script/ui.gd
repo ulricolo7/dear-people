e3745835237e0eb4c0eb41dec2d328b4e3e8ea10
ulricolo7 <ulricoco39lo7@gmail.com>
2024-11-04 12:24:23 +0800

    Add Proper Dialog

--- Script/ui.gd
+++ Script/ui.gd
@@ -13,8 +13,11 @@ var curr_npc
 var npc_alex
 var npc_alex_dialog
 var npc_alex_ans
+var npc_alex_val = 0
 var npc_mary
 var npc_mary_dialog
+var npc_mary_ans
+var npc_mary_val = 0
 
 var in_dialog = false
 
@@ -55,16 +58,20 @@ func parse(str):
 	elif str == "hi" && !in_dialog:
 		var npc = player.check_person()
 		interact(npc, 0)
-	elif str == "bye" && in_dialog:
-		interact(curr_npc, -1)
-		in_dialog = false
+	
 
 func dialog_parse(str):
 	if curr_npc == "Alex":
-		interact("Alex", 1)
+		if str == "bye":
+			interact(curr_npc, -1)
+			in_dialog = false
+		else:
+			npc_alex_val += 1
+			interact(curr_npc, npc_alex_val)
 
 func interact(npc, i):
 	#ADD NPC
+	curr_npc = npc
 	if npc == "no one":
 		return
 	elif npc == "Alex":
@@ -72,18 +79,23 @@ func interact(npc, i):
 	elif npc == "Mary":
 		set_mary(i)
 		
-	in_dialog = true
-	curr_npc = npc
-	player.toggle_movement()
+	
 
 #ADD NPC
 func set_alex(i):
 	if i < 0:
 		npc_alex.position = STANDBY_POS
+		player.toggle_movement()
 	elif i == 0:
 		npc_alex.position = ACTIVE_POS
+		player.toggle_movement()
+		in_dialog = true
 	elif i == 1:
-		npc_alex_dialog.text = "Does it work?\n\n" + "huh"
+		npc_alex_dialog.text = "You'll be fine.\n\n" + "Don't worry\ntoo much"
+		npc_alex_ans.text = "> \"ok\"\n" + "> \"nervous\""
+	else:
+		npc_alex_dialog.text = "You can say\nbye now."
+		npc_alex_ans.text = "> \"bye\"\n"
 
 func set_mary(i):
 	if i < 0:
